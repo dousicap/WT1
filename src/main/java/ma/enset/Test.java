62fdc67c3feb7e4e137ab0818d8bd8c23842f70e
Dou <papamadouba@gmail.com>
2021-09-16 19:03:52 +0200

    Comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,10 @@
+/**
+ * Comment
+ */
 package ma.enset;   
 public class Test {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
+        //-----------------------------------
         System.out.println("Hello!");
     }
 }
